d5beceac9b454656d01f107064ac2521a8f71fed
maoenpei <patronumme@gmail.com>
2015-03-03 15:35:59 +0800

    del delegate

--- src/ctrl/CDelegate.cpp
+++ src/ctrl/CDelegate.cpp
@@ -10,69 +10,6 @@
 #include "engine/CProjectionNode.h"
 #include "engine/CSpriteNode.h"
 
-const char vertexShader[] = 
-"attribute vec2 in_pos;\n"
-"attribute vec2 in_tex;\n"
-"uniform vec2 uni_trans[3];\n"
-"varying vec2 var_tex_coord;\n"
-"void main(){\n"
-"	gl_Position = vec4(\n"
-"in_pos.x*uni_trans[0].x+in_pos.y*uni_trans[1].x+uni_trans[2].x, \n"
-"in_pos.x*uni_trans[0].y+in_pos.y*uni_trans[1].y+uni_trans[2].y, 0, 1);\n"
-"	var_tex_coord = in_tex;\n"
-"}\n"
-;
-
-const char fragShader[] = 
-"uniform sampler2D uni_tex_id;\n"
-"varying vec2 var_tex_coord;\n"
-"void main(){\n"
-"	gl_FragColor = vec4(1.0, 0, 0, 1.0);\n"
-"}\n"
-;
-
-struct POSITION
-{
-	float x,y;
-	POSITION(float _x, float _y) : x(_x), y(_y){}
-	POSITION() : x(0), y(0){}
-};
-
-class TestNode : public engine::CSceneNode
-{
-public:
-
-	TestNode(view::IPlatform *platform)
-		: CSceneNode(platform)
-	{
-		pos[0] = POSITION(0.f, 0.f);
-		pos[1] = POSITION(200.f, 0.f);
-		pos[2] = POSITION(0.f, 200.f);
-		pos[3] = POSITION(200.f, 200.f);
-		idxs[0] = 0;
-		idxs[1] = 1;
-		idxs[2] = 2;
-		idxs[3] = 1;
-		idxs[4] = 3;
-		idxs[5] = 2;
-		core::TAuto<graphics::IShaderProgram> program;
-		G->loadProgram(program, vertexShader, fragShader);
-		program->setTransformName("uni_trans");
-		unsigned int l_pos = program->getAttributeIndex("in_pos");
-		program->createDrawer(Drawer);
-		Drawer->setAttributeValue(l_pos, 0, 2, SIG_FLOAT);
-		Drawer->setAttributeData(&pos, 4, sizeof(pos[0]));
-		Drawer->setIndexs(idxs, 6, SIG_USHORT);
-
-//		Camera = new engine::CNodeCamera();
-//		Camera->rotate(-3.141592653f / 6);
-//		Camera->scale(1/400.f, 1/240.f);
-//		Camera->move(100, 100);
-	}
-	POSITION pos[4];
-	unsigned short idxs[6];
-};
-
 namespace ctrl{
 
 CDelegate::CDelegate()
@@ -81,7 +18,6 @@ CDelegate::CDelegate()
 
 void CDelegate::onInitialized(view::IPlatform *platform)
 {
-//	TestNode *node = new TestNode(platform);
 	engine::CSpriteNode *node = new engine::CSpriteNode(platform, "small_arrow.png");
 	node->attachColor(model::Color3f(1, 0, 0));
 	engine::CProjectionNode *project = new engine::CProjectionNode(platform);
